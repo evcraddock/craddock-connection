4680198fbc00f2c38946203df54af42e40f47409
Erik Craddock <erik@craddock.org>
2024-05-29 17:47:19 -0500

    updates packages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,11 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
 
 import react from "@astrojs/react";
 
 export default defineConfig({
   site: 'https://craddock.org',
-  integrations: [mdx(), sitemap(), tailwind(), react()]
+  integrations: [mdx(), sitemap(), tailwind(), react(), icon()]
 });
