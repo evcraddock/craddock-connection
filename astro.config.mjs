6117a59e5ad49c3bd0f79e99bf2589fa551f362e
Erik Craddock <erik@craddock.org>
2024-07-17 20:06:28 -0500

    updates default site url

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,19 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
 
 import react from "@astrojs/react";
 
 export default defineConfig({
-  site: 'https://craddock.org',
+  site: "https://www.craddock.org",
   integrations: [mdx(), sitemap(), tailwind(), react(), icon()],
   vite: {
     resolve: {
       alias: {
-        '/images': '/images',
-      }
-    }
-  }
-
+        "/images": "/images",
+      },
+    },
+  },
 });
